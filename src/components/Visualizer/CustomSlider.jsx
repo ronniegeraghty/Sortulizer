6e8880c345f2ab7b501019224f3b134377b15054
Ronnie Geraghty <ronniegerag@gmail.com>
2020-06-04 17:06:14 -0400

    added slider

---
+++ src/components/Visualizer/CustomSlider.jsx
@@ -0,0 +1,35 @@
+import { withStyles } from "@material-ui/core/styles";
+import Slider from "@material-ui/core/Slider";
+
+const styles = {
+  root: {
+    color: "#52af77",
+    height: 8,
+    width: "30vw",
+  },
+  thumb: {
+    height: 24,
+    width: 24,
+    backgroundColor: "#fff",
+    border: "2px solid currentColor",
+    marginTop: -8,
+    marginLeft: -12,
+    "&:focus, &:hover, &$active": {
+      boxShadow: "inherit",
+    },
+  },
+  active: {},
+  valueLabel: {
+    left: "calc(-50% + 4px)",
+  },
+  track: {
+    height: 8,
+    borderRadius: 4,
+  },
+  rail: {
+    height: 8,
+    borderRadius: 4,
+  },
+};
+
+export default withStyles(styles)(Slider);
--- src/components/Visualizer/index.jsx
+++ src/components/Visualizer/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./visualizer.css";
+import CustomSlider from "./CustomSlider";
 import Bar from "../Bar";
 
 export default function Visualizer({
@@ -9,6 +10,7 @@ export default function Visualizer({
   comparisons,
 }) {
   const [barWidth, setBarWidth] = useState(null);
+  const [sortSpeed, setSortSpeed] = useState(100);
 
   useEffect(() => {
     setBarWidth(90 / array.length);
@@ -24,6 +26,20 @@ export default function Visualizer({
       <div className="row">
         <p className="info-data">Comparisons: {comparisons}</p>
       </div>
+      <div className="row">
+        <p className="info-data sort-speed-data">Sort Speed: </p>
+        <CustomSlider
+          className="sort-speed-data"
+          valueLabelDisplay="auto"
+          min={0}
+          max={100}
+          value={sortSpeed}
+          onChange={(e, value) => {
+            setSortSpeed(value);
+          }}
+        />
+      </div>
+
       <div className="bar-row row">
         {array.map((value, index) => (
           <Bar
