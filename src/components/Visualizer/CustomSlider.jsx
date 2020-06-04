71b03f31a245ee8b1392a7b8f4d9a253b890082b
Ronnie Geraghty <ronniegerag@gmail.com>
2020-06-04 17:55:55 -0400

    Worked on slider styling

--- src/components/Visualizer/CustomSlider.jsx
+++ src/components/Visualizer/CustomSlider.jsx
@@ -1,35 +1,47 @@
 import { withStyles } from "@material-ui/core/styles";
 import Slider from "@material-ui/core/Slider";
 
+const sizer = 3;
+function calcLabelLeft(x) {
+  return (
+    -((257 * x * x * x * x) / 420) +
+    (72 * x * x * x) / 5 -
+    (51217 * x * x) / 420 +
+    (4629 * x) / 10 -
+    4854 / 7
+  );
+}
+
 const styles = {
   root: {
     color: "#52af77",
-    height: 8,
+    height: 2 * sizer,
     width: "30vw",
   },
   thumb: {
-    height: 24,
-    width: 24,
+    height: 6 * sizer,
+    width: 6 * sizer,
     backgroundColor: "#fff",
     border: "2px solid currentColor",
-    marginTop: -8,
-    marginLeft: -12,
+    marginTop: -2 * sizer,
+    marginLeft: -3 * sizer,
     "&:focus, &:hover, &$active": {
       boxShadow: "inherit",
     },
   },
   active: {},
   valueLabel: {
-    left: "calc(-50% + 4px)",
+    left: calcLabelLeft(sizer) + "%",
   },
   track: {
-    height: 8,
-    borderRadius: 4,
+    height: 2 * sizer,
+    borderRadius: sizer,
   },
   rail: {
-    height: 8,
-    borderRadius: 4,
+    height: 2 * sizer,
+    borderRadius: sizer,
   },
 };
 
 export default withStyles(styles)(Slider);
+//-7 * (16 / sizer) + "%",
