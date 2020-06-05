ecad4baf7ea9d37d80dc1b322b25c8d067bb07b3
Ronnie Geraghty <ronniegerag@gmail.com>
2020-06-05 17:19:35 -0400

    fixed test error

--- src/components/Sound/index.jsx
+++ src/components/Sound/index.jsx
@@ -2,17 +2,24 @@ import React, { useEffect } from "react";
 import Pizzicato from "pizzicato";
 
 function Sound({ status, soundFreqs }) {
-  let soundGroup = new Pizzicato.Group([]);
-  soundFreqs.forEach(soundFreq => {
-    let sound = new Pizzicato.Sound({
-      source: "wave",
-      options: {
-        type: "square",
-        frequency: soundFreq,
-      },
+  let soundGroup = {
+    play: () => {},
+    stop: () => {},
+  };
+  try {
+    soundGroup = new Pizzicato.Group([]);
+    soundFreqs.forEach(soundFreq => {
+      let sound = new Pizzicato.Sound({
+        source: "wave",
+        options: {
+          type: "square",
+          frequency: soundFreq,
+        },
+      });
+      soundGroup.addSound(sound);
     });
-    soundGroup.addSound(sound);
-  });
+  } catch (error) {}
+
   //   function playSound() {
   //     soundGroup.play();
   //     setTimeout(() => {
@@ -27,7 +34,7 @@ function Sound({ status, soundFreqs }) {
     return () => {
       soundGroup.stop();
     };
-  }, [soundGroup, status]);
+  }, [status, soundGroup]);
 
   return <div> {/* <button onClick={playSound}>Play</button> */}</div>;
 }
